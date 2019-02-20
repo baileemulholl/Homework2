c7b295ea690e1440d2687326b1b0445511c5a17e
bailee <bailee.maezuo@gmail.com>
2019-02-20 14:35:55 -0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,15 @@ using namespace std;
 #include "TextUI.h"
 #include "Item.h"
 
-int main(){
+int main(int argc, char *argv[]){
+	if(argc == 2){
+		TextUI menu;
+		Store s(argv[1]);
+		string choice;
+		while(choice != "Leave"){
+			choice = menu.MainMenu();
+			menu.RouteChoice(s,choice);
+		}
+	}
 	return 0;
 }
